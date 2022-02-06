ae076679c4b80cc695cca2bad804afba57e5d66e
aysegulyilmaz5 <83874900+aysegulyilmaz5@users.noreply.github.com>
2022-02-07 01:41:43 +0300

    if-else

---
+++ if_else.dart
@@ -0,0 +1,25 @@
+void main(List<String> args){
+  
+  int number1 = 12;
+  int number2 = 10;
+  
+  if(number1 > number2){
+    print("$number1 is greater than $number2");
+  }
+  else{
+    print("$number1 is less than $number2");
+  }
+  
+  int number3= 14;
+  int number4 = 14;
+  
+  if(number3<number4){
+    print("$number3 less than $number4");
+  }
+  else if(number3 > number4){
+    print("$number3 greater than $number4");
+  }
+  else{
+    print("$number3 equal to $number4");
+  }
+}
